5b844fee88c4cbdc88158f234fbcc02b42ddaccc
dabhijeet <44795831+dabhijeet@users.noreply.github.com>
2019-12-22 03:13:07 -0800

    Add files via upload

---
+++ mecanum_wheel.ino
@@ -0,0 +1,246 @@
+//电机引脚
+#define PWMA 3 //A电机转速
+#define DIRA 2 //A电机转向
+#define PWMB 5 //B电机转速
+#define DIRB 4 //B电机转向
+#define PWMC 6 //A电机转速
+#define DIRC 7 //A电机转向
+#define PWMD 9 //B电机转速
+#define DIRD 8 //B电机转向
+//控制电机运动    宏定义
+int i=2;
+void ANTICLOCKWISE_ROTATION()
+{
+  digitalWrite(DIRA,HIGH);
+  digitalWrite(DIRB,HIGH);
+  digitalWrite(DIRC,HIGH);
+  digitalWrite(DIRD,HIGH);
+
+  analogWrite(PWMA,150);
+  analogWrite(PWMB,150);
+  analogWrite(PWMC,150);
+  analogWrite(PWMD,150);
+}
+void DRIFT_LEFT()
+{
+  digitalWrite(DIRA,HIGH);
+  digitalWrite(DIRC,HIGH);
+  
+  analogWrite(PWMA,150);
+  analogWrite(PWMC,150);
+
+  analogWrite(PWMB,0);
+  analogWrite(PWMD,0);
+}
+void RIGHT()
+{
+  i=2;
+  digitalWrite(DIRA,HIGH);
+  digitalWrite(DIRB,HIGH);
+  digitalWrite(DIRC,LOW);
+  digitalWrite(DIRD,LOW);
+  while(i<=128)
+  {
+  analogWrite(PWMA,i);
+  analogWrite(PWMB,i);
+  analogWrite(PWMC,i);
+  analogWrite(PWMD,i);
+  i=i*2;
+  delay(25);
+  }
+  analogWrite(PWMA,150);
+  analogWrite(PWMB,150);
+  analogWrite(PWMC,150);
+  analogWrite(PWMD,150);
+}
+void FORWARD()
+{
+  i=2;
+  digitalWrite(DIRA,HIGH);
+  digitalWrite(DIRB,LOW);
+  digitalWrite(DIRC,HIGH);
+  digitalWrite(DIRD,LOW);
+  while(i<=128)
+  {
+  analogWrite(PWMA,i);
+  analogWrite(PWMB,i);
+  analogWrite(PWMC,i);
+  analogWrite(PWMD,i);
+  i=i*2;
+  delay(25);
+  }
+  analogWrite(PWMA,150);
+  analogWrite(PWMB,150);
+  analogWrite(PWMC,150);
+  analogWrite(PWMD,150);
+}
+void DRIFT_FRONT()
+{
+  digitalWrite(DIRA,HIGH);
+  digitalWrite(DIRB,HIGH);
+  
+  analogWrite(PWMA,150);
+  analogWrite(PWMB,150);
+
+  analogWrite(PWMC,0);
+  analogWrite(PWMD,0);
+}
+void LEFT()
+{
+  i=2;
+  digitalWrite(DIRA,LOW);
+  digitalWrite(DIRB,LOW);
+  digitalWrite(DIRC,HIGH);
+  digitalWrite(DIRD,HIGH);
+  while(i<=128)
+  {
+  analogWrite(PWMA,i);
+  analogWrite(PWMB,i);
+  analogWrite(PWMC,i);
+  analogWrite(PWMD,i);
+  i=i*2;
+  delay(25);
+  }
+  analogWrite(PWMA,150);
+  analogWrite(PWMB,150);
+  analogWrite(PWMC,150);
+  analogWrite(PWMD,150);
+}
+void FORWARD_LEFT()
+{
+  i=2;
+  digitalWrite(DIRB,LOW);
+  digitalWrite(DIRC,HIGH);
+  while(i<=128)
+  {
+  analogWrite(PWMB,i);
+  analogWrite(PWMC,i);
+  i=i*2;
+  delay(25);
+  }
+  analogWrite(PWMA,0);
+  analogWrite(PWMD,0);
+  analogWrite(PWMB,150);
+  analogWrite(PWMC,150);
+}
+void FORWARD_RIGHT()
+{
+  i=2;
+  digitalWrite(DIRA,HIGH);
+  digitalWrite(DIRD,LOW);
+  while(i<=128)
+  {
+  analogWrite(PWMA,i);
+  analogWrite(PWMD,i);
+  i=i*2;
+  delay(25);
+  }
+  analogWrite(PWMB,0);
+  analogWrite(PWMC,0);
+  analogWrite(PWMA,150);
+  analogWrite(PWMD,150);
+}
+void BACKWARD()
+{
+  i=2;
+  digitalWrite(DIRA,LOW);
+  digitalWrite(DIRB,HIGH);
+  digitalWrite(DIRC,LOW);
+  digitalWrite(DIRD,HIGH);
+  while(i<=128)
+  {
+  analogWrite(PWMA,i);
+  analogWrite(PWMB,i);
+  analogWrite(PWMC,i);
+  analogWrite(PWMD,i);
+  i=i*2;
+  delay(25);
+  }
+  analogWrite(PWMA,150);
+  analogWrite(PWMB,150);
+  analogWrite(PWMC,150);
+  analogWrite(PWMD,150);
+}
+void STOP()
+{
+  digitalWrite(DIRA,LOW);
+  digitalWrite(DIRB,LOW);
+  digitalWrite(DIRC,LOW);
+  digitalWrite(DIRD,LOW);
+  
+  analogWrite(PWMA,0);
+  analogWrite(PWMB,0);
+  analogWrite(PWMC,0);
+  analogWrite(PWMD,0);
+}
+void UART_Control()
+{
+  char Uart_Date=0;
+  if(Serial.available())
+  {
+    Uart_Date = Serial.read();
+  }
+  if(Uart_Date=='A')
+  {
+    ANTICLOCKWISE_ROTATION();
+  }
+  if(Uart_Date=='R')
+  {
+    RIGHT();
+  }
+  if(Uart_Date=='F')
+  {
+    FORWARD();
+  }
+  if(Uart_Date=='T')
+  {
+    DRIFT_FRONT();
+  }
+  if(Uart_Date=='B')
+  {
+    BACKWARD();
+  }
+  if(Uart_Date=='U')
+  {
+    FORWARD_LEFT();
+  }
+  if(Uart_Date=='V')
+  {
+    FORWARD_RIGHT();
+  }
+  if(Uart_Date=='L')
+  {
+    LEFT();
+  }
+  if(Uart_Date=='C')
+  {
+    DRIFT_LEFT();
+  }
+  if(Uart_Date=='S')
+  {
+    STOP();
+  }
+}
+void IO_init()
+{
+  pinMode(PWMA, OUTPUT);
+  pinMode(DIRA, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+  pinMode(DIRB, OUTPUT);
+  pinMode(PWMC, OUTPUT);
+  pinMode(DIRC, OUTPUT);
+  pinMode(PWMD, OUTPUT);
+  pinMode(DIRD, OUTPUT);
+  STOP();
+}
+void setup()
+{
+  Serial.begin(9600);
+  IO_init();
+}
+
+void loop()
+{ 
+  UART_Control();//串口接收处理 
+  //CAR_Control();//小车控制
+}
